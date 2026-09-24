e1ca04d4912296dc23c62778ac7e90c0ecb7cd74
Jason-git-shu <a115222002@mail.shu.edu.tw>
2026-09-24 18:00:59 +0800

    2026-09-24

--- 115_09_24/Tutorial_2_2/Tutorial_2_2/Form1.cs
+++ 115_09_24/Tutorial_2_2/Tutorial_2_2/Form1.cs
@@ -16,5 +16,9 @@ namespace Tutorial_2_2
         {
             InitializeComponent();
         }
+        private void messageButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("中秋快樂");
+        }
     }
 }
